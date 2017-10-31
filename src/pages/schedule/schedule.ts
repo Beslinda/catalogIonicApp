7298341bb7e85489c2e1ec908989dd54728f826b
besa23 <beslinda.neziri@gmail.com>
2017-10-31 22:43:25 +0100

    Customize template

--- src/pages/schedule/schedule.ts
+++ src/pages/schedule/schedule.ts
@@ -53,7 +53,7 @@ export class SchedulePage {
   updateSchedule() {
     // Close any open sliding items when the schedule updates
     this.scheduleList && this.scheduleList.closeSlidingItems();
-
+    // this.segment = 'favorites';
     this.confData.getTimeline(this.dayIndex, this.queryText, this.excludeTracks, this.segment).subscribe((data: any) => {
       this.shownSessions = data.shownSessions;
       this.groups = data.groups;
